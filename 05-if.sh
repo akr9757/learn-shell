93b9975cd1295bcbf67e04bce52cc533478c98d1
akr9757 <anilkumareddy9757@gmail.com>
2023-07-22 05:59:12 +0530

    if condition

--- 05-if.sh
+++ 05-if.sh
@@ -5,8 +5,6 @@ quantity=$2
 if [ "$fruit_name" == mango ]; then
   echo mango quantity = $quantity
 else
-  echo fruit does not exit
-else
   if [ "$fruit_name" == apple ]; then
     echo apple quantity = $quantity
 fi
